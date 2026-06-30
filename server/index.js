07c4c1bd9d24dca863216517703fcb0d9a5b7031
Piyush Kumar Sharma <piyushsharma1817@gmail.com>
2026-06-30 19:44:10 +0530

    fix port for render

--- server/index.js
+++ server/index.js
@@ -8,5 +8,5 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', analyzeRoute);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
